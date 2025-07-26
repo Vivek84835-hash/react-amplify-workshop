efd4eabc37fdb3f1905cbb5ffa850aac9ba58fda
Vivek84835-hash <vivekparekh2204@gmail.com>
2025-07-26 12:20:58 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 // export const endpoint = 'http://15.206.164.156:8080';
-export const endpoint = 'https://d7vf3cvfu7.execute-api.ap-south-1.amazonaws.com';
+export const endpoint = 'https://d7vf3cvfu7.execute-api.ap-south-1.amazonaws.com/';
 
 export const API_PATH = '/api/users';
